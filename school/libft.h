607bb7c91221c3cef90d39f6645ada26dd44761f
super-kimchi <powercoding1004@gmail.com>
2024-11-15 23:55:38 +0100

    tolower

--- school/libft.h
+++ school/libft.h
@@ -30,4 +30,5 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t  ft_strlcpy(char *dest, const char *src, size_t dstsize);                   
 size_t  ft_strlcat(char *dest, const char *src, size_t dstsize);
 int ft_toupper(int c);
+int ft_tolower(int c)
 #endif
