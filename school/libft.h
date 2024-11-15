d79f17781971e1b711755fdbb8e6ecbf86220a10
super-kimchi <powercoding1004@gmail.com>
2024-11-15 23:51:43 +0100

    ft_toupper

--- school/libft.h
+++ school/libft.h
@@ -29,4 +29,5 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t  ft_strlcpy(char *dest, const char *src, size_t dstsize);                   
 size_t  ft_strlcat(char *dest, const char *src, size_t dstsize);
+int ft_toupper(int c);
 #endif
