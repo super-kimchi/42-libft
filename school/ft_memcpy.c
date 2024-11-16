df2787d712db7344e7ad60d45475c6b7eccd822f
fermented-kimchi <rotten_kimchi@kali.Demokali.local>
2024-11-16 11:21:48 +0530

    update

--- school/ft_memcpy.c
+++ school/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: kyungkim <kyungkim@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 18:38:41 by kyungkim          #+#    #+#             */
-/*   Updated: 2024/11/13 20:12:15 by kyungkim         ###   ########.fr       */
+/*   Updated: 2024/11/16 06:25:49 by kyungkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char		*start;
 	const unsigned char	*start_src;
 
-	if (dest == NULL || src == NULL)
-		return NULL;
 	start = dest;
 	start_src = src;
+	if (dest == NULL && src == NULL)
+		return (dest);
 	while (n > 0)
 	{
 		*start = *start_src;
@@ -30,7 +30,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	}
 	return (dest);
 }
-
+/*
 #include <string.h>
 int	main(void)
 {
@@ -47,7 +47,9 @@ int	main(void)
 	printf("\n:%s:\n", arr1);
 	ft_memcpy(arr1, "AA", 2);
 	printf(":%s:\n", arr1);
-
-	printf("%p", memcpy((void *)0, (void *)0, 3));
+	char arr2[] = "hi42kay";
+	printf("%p", ft_memcpy(arr1, (void *)0, 3));
+	//printf("%p", memcpy((void *)0, (void *)0, 3));
+	
 }
-
+*/
--- school/ft_memmove.c
+++ school/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: kyungkim <kyungkim@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 20:20:06 by kyungkim          #+#    #+#             */
-/*   Updated: 2024/11/13 22:08:53 by kyungkim         ###   ########.fr       */
+/*   Updated: 2024/11/16 06:59:43 by kyungkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,32 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char	*start;
 	unsigned char	*start_src;
+	int	c;
 
-	start = (unsigned char *)dest + n - 1;
-	start_src = (unsigned char *)src + n - 1;
+	if (dest == NULL && src == NULL)
+		return (dest);
+	if (dest - src > 0)
+	{
+		start = (unsigned char *)dest + n - 1;
+		start_src = (unsigned char *)src + n - 1;
+	}
+	else
+	{
+		start = (unsigned char *)dest;
+		start_src = (unsigned char *)src;
+	}
 	while (n > 0)
 	{
-		*start = *start_src;
-		start--;
-		start_src--;
+		start[c] = start_src[c];
+		if (dest - src > 0)
+			c--;
+		else
+			c++;
 		n--;
 	}
 	return (dest);
 }
-/*
+
 #include <string.h>
 
 int	main(void)
@@ -44,5 +57,23 @@ int	main(void)
 	printf("real %s\n", dest);
 	ft_memmove(dest, arr, 4);
 	printf("my %s\n", dest);
+
+	char dst1[0xF0];
+    char dst2[0xF0];
+    char *data="zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz";
+    int size = 0xF0 - 0xF;
+ 
+    memset(dst1, 'A', sizeof(dst1));
+    memset(dst2, 'A', sizeof(dst2));
+    memcpy(dst1, data, strlen(data));
+    memcpy(dst2, data, strlen(data));
+	printf("%s\n", dst1);
+	printf("%s\n", dst1);
+	printf("%d\n", size);
+    memmove(dst1, dst1 + 3, size);
+    ft_memmove(dst2, dst2 + 3, size);
+	printf("%s\n", dst1);
+	printf("%s\n", dst1);
+
 }
-*/
+
