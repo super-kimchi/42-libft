3aa6625b30f1a68fae964876abbc0875c145775b
42-kimchi <powercoding1004@gmail.com>
2024-11-18 19:44:46 +0100

    deleting useless stuff

--- Makefile
+++ Makefile
@@ -1,18 +1,21 @@
 NAME = libft.a
 CC = cc
-CFLAGS = -Wextra -Werror -Wall
-SRCS = $(wildcard *.c)
+CFLAGS = -Wall -Wextra -Werror
+
+SRCS = $(wildcard ft_*.c)
 OBJ = $(SRCS:.c=.o)
 
 all: $(NAME)
 
 $(NAME): $(OBJ)
 	ar rcs $(NAME) $(OBJ)
-%.o: %.c
-	$(CC) -c $(CFLAGS) $< -o $@
+
+%.o: %.c		
+	$(CC) -c $(CFLAGS) $<
 clean:
 	rm *.o
+
 fclean: clean
 	rm $(NAME)
+
 re: fclean all
-	
--- libft.h
+++ libft.h
@@ -3,22 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kyungkim <kyungkim@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: kyungkim <kyungkim@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/15 02:07:50 by kyungkim          #+#    #+#             */
-/*   Updated: 2024/11/15 04:07:11 by kyungkim         ###   ########.fr       */
+/*   Created: 2024/11/12 15:44:05 by kyungkim          #+#    #+#             */
+/*   Updated: 2024/11/16 05:55:17 by kyungkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
+#ifndef LIBFT_H 
 # define LIBFT_H
 
+# include <stdio.h>
 # include <stddef.h>
 # include <bsd/string.h>
-# include <stdio.h>
 
-size_t	ft_strlen(const char *str);
-size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dest, const char *src, size_t dstsize);
-
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 #endif
