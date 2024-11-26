1ddcb7098ccbb7f81529f06761ade10fd1f17fd2
super-kimchi <powercoding1004@gmail.com>
2024-11-27 01:39:02 +0530

    split fix

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: kyungkim <kyungkim@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 18:51:38 by kyungkim          #+#    #+#             */
-/*   Updated: 2024/11/26 12:19:41 by kyungkim         ###   ########.fr       */
+/*   Updated: 2024/11/27 01:38:21 by kyungkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,8 @@ char	*seperator(char const *s, int sep)
 	int		i;
 
 	c = 0;
-	while (s[c] != sep && s[c + 1])
+	while (s[c] != sep && s[c])
 		c++;
-//	if (c == 0)
-//		c = ft_strlen(s);
 	str = (char *)malloc(c + 1);
 	i = 0;
 	while (i < c)
@@ -34,50 +32,59 @@ char	*seperator(char const *s, int sep)
 	return (str);
 }
 
-char **ft_split(char const *s, char c)
+int	count(char const *s, char c)
 {
-	char	**arr;
-	int	sep_c;
 	int	i;
-	int	arr_c;
-	
-	sep_c = 0;
+	int	sep_c;
+
 	i = 0;
+	sep_c = 0;
+	if (ft_strlen(s) == 0)
+		return (0);
 	while (s[i])
 	{
 		if (s[i] != c)
 		{
-				sep_c++;
-				while(s[i] != c)
-					i++;
+			sep_c++;
+			while (s[i] != c && s[i])
+				i++;
 		}
 		else
 			i++;
 	}
-	if (ft_strlen(s) == 0)
-		sep_c = 0;
-	arr = (char **)malloc(sep_c + 1);
+	return (sep_c);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**arr;
+	int		sep_c;
+	int		i;
+	int		arr_c;
+
+	sep_c = count(s, c);
+	arr = (char **)malloc((sep_c + 1) * sizeof(char *));
 	if (!arr)
 		return (NULL);
 	i = 0;
 	arr_c = 0;
 	while (arr_c < sep_c)
-	{	
+	{
 		while (*s == c)
 			s++;
 		arr[arr_c] = seperator(s, c);
-		s = s + ft_strlen(arr[arr_c]) + 1;
+		s = s + ft_strlen(arr[arr_c]);
 		arr_c++;
 	}
 	arr[arr_c] = 0;
 	return (arr);
 }
-
+/*
 int	main(void)
 {
 //	char	**test = ft_split("hi 42 kay", ' ');
 //	char	**test1 = ft_split("", ' ');
-	char	**test2 = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse", ' ');
+	char	**test2 = ft_split("xxxxxxxxhello!", 'x');
 
 //	for (int i = 0; i < 4; i++)
 //		printf(":%s: ", test[i]);
@@ -85,7 +92,7 @@ int	main(void)
 //	for (int i = 0; i < 1; i++)
 //		printf(":%s: ", test1[i]);
 //	printf("\n");
-	for (int i = 0; i < 13; i++)
+	for (int i = 0; i < 2; i++)
 		printf(":%s: ", test2[i]);
 }
-
+*/
