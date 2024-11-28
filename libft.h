16289944b1c1e7647012a4296fd57727e62f6989
super-kimchi <powercoding1004@gmail.com>
2024-11-28 17:24:32 +0530

    ft_lstsize ft_lstlast

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kyungkim <kyungkim@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 15:44:05 by kyungkim          #+#    #+#             */
-/*   Updated: 2024/11/28 12:43:33 by kyungkim         ###   ########.fr       */
+/*   Updated: 2024/11/28 13:29:22 by kyungkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,4 +64,6 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 #endif
