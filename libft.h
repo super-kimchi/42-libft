c9712b1188db027756bae0613c7c3a2d6453e17c
super-kimchi <powercoding1004@gmail.com>
2024-12-02 10:59:30 +0100

    ft_lstclear

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kyungkim <kyungkim@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 15:44:05 by kyungkim          #+#    #+#             */
-/*   Updated: 2024/12/02 08:59:50 by kyungkim         ###   ########.fr       */
+/*   Updated: 2024/12/02 09:52:09 by kyungkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,4 +68,5 @@ int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
 #endif
